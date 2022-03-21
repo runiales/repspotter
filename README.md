8066132c0dc5c7faa2efcf50f1479a0970fe7ef0
runiales <irpic.am@gmail.com>
2022-03-21 18:25:47 +0100

    primer commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# RepSpotter
+
+Monitoriza las repeticiones y los sets de cada ejercicio. Analiza las bases de datos con python.
+
+## TODO:
+
++ [ ]
---
+++ reps.c
@@ -0,0 +1,14 @@
+/* opción para preguntar por cómo me siento */
+
+// empezar temporizador
+
+/* pregunta dips */
+
+/* pregunta dominadas */
+
+/* guarda en archivo con fecha */
+
+/* compara con otras fechas */
+
+/* opción para notas adicionales (también adjuntas a archivo) */
+
